99345f381db986f1d88643bc3eea571b2ccad901
NikiRenegade <nikirenegade@gmail.com>
2025-11-07 13:15:30 +0700

    feat: add language and theme switcher

--- src/components/headerComponent/Header.tsx
+++ src/components/headerComponent/Header.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import './header.scss';
+import React, { useContext } from 'react';
+import styles from './header.module.scss';
 import { Logo } from '../logoComponent/Logo';
-
+import { ThemeSwitcher } from '../themeSwitcherComponent/ThemeSwitcher';
+import { LanguageSwitcher } from '../languageSwitcherComponent/LanguageSwitcher';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import { LanguageContext } from '../../contexts/LanguageContext';
+import { useTranslation } from 'react-i18next';
 export function Header() {
+  const { theme } = useContext(ThemeContext);
+  const { language } = useContext(LanguageContext);
+  const { t } = useTranslation();
   return (
-    <header className="header">
-      <div className="header__logo">
+    <header className={`${styles.header} ${styles[theme]}`} data-test={theme}>
+      <div className={styles['header__logo']}>
         <Logo />
       </div>
-      <nav className="header__nav">
-        <a className="header__link" href="/">Главная</a>
-        <a className="header__link" href="/login">Вход</a>
-        <a className="header__link" href="/register">Регистрация</a>
+      <nav className={styles['header__nav']}>
+        <a className={styles['header__nav__link']} href="/">
+          {t('home')}
+        </a>
+        <a className={styles['header__nav__link']} href="/login">
+          {t('login')}
+        </a>
+        <a className={styles['header__nav__link']} href="/register">
+          {t('register')}
+        </a>
       </nav>
+      <ThemeSwitcher />
+      <LanguageSwitcher />
     </header>
   );
 }
